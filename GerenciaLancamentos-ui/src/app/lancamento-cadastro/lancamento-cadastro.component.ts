8e567548dd586c08e133a550437d6802d354e760
TiagoTSR <craimos3001@gmail.com>
2024-11-17 08:08:37 -0300

    Componente lancamento-cadastro

---
+++ GerenciaLancamentos-ui/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrl: './lancamento-cadastro.component.scss'
+})
+export class LancamentoCadastroComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
