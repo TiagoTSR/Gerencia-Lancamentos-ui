8cb122579b9e9fa0008e5fdd258bcf6fdefd759b
TiagoTSR <craimos3001@gmail.com>
2024-11-18 08:21:26 -0300

    Formulário pessoa-cadastro

---
+++ GerenciaLancamentos-ui/src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-cadastro',
+  templateUrl: './pessoa-cadastro.component.html',
+  styleUrl: './pessoa-cadastro.component.scss'
+})
+export class PessoaCadastroComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
