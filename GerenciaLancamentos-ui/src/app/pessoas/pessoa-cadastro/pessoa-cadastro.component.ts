aafc7ad77b61fb97986b46145ba2951b77b1c891
TiagoTSR <craimos3001@gmail.com>
2025-01-02 08:57:00 -0300

    Implementando o cadastro de pessoas

--- GerenciaLancamentos-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ GerenciaLancamentos-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+
+import { MessageService } from 'primeng/api';
+
+
+import { PessoaService } from '../pessoa.service';
+import { Pessoa } from '../../core/model';
+import { ErrorHandlerService } from '../../core/error-handler.service';
 
 @Component({
   selector: 'app-pessoa-cadastro',
   templateUrl: './pessoa-cadastro.component.html',
-  styleUrl: './pessoa-cadastro.component.scss'
+  styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent {
+export class PessoaCadastroComponent implements OnInit {
 
-  constructor() { }
+  pessoa = new Pessoa();
 
-  ngOnInit(): void {
+  constructor(
+    private pessoaService: PessoaService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
   }
+
+  salvar(form: NgForm) {
+    this.pessoaService.adicionar(this.pessoa)
+      .then(() => {
+        this.messageService.add({ severity: 'success', detail: 'Pessoa adicionada com sucesso!' });
+
+        form.reset();
+        this.pessoa = new Pessoa();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
 }
