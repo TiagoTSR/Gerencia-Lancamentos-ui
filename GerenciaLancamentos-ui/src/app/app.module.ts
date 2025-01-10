aee478c4032cab7f5ae784d194915a986f94b617
TiagoTSR <craimos3001@gmail.com>
2025-01-10 14:31:01 -0300

    Fazendo redirecionamento

--- GerenciaLancamentos-ui/src/app/app.module.ts
+++ GerenciaLancamentos-ui/src/app/app.module.ts
@@ -27,9 +27,10 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 }
 
 const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
   { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
   { path: 'pessoas', component: PessoasPesquisaComponent }
 ];
 
