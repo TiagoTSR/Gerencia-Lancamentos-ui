e606146a56ee1c81aaa4bfe8a6f3c98fa19115fe
TiagoTSR <craimos3001@gmail.com>
2025-01-26 07:34:53 -0300

    Criando validações customizadas

--- GerenciaLancamentos-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ GerenciaLancamentos-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
@@ -61,7 +61,7 @@ export class LancamentoCadastroComponent implements OnInit {
       tipo: [ 'RECEITA', Validators.required ],
       dataVencimento: [ null, Validators.required ],
       dataPagamento: [],
-      descricao: [null, [ Validators.required, Validators.minLength(5) ]],
+      descricao: [null, [ this.validarObrigatoriedade, this.validarTamanhoMinimo(5) ]],
       valor: [ null, Validators.required ],
       pessoa: this.formBuilder.group({
         codigo: [ null, Validators.required ],
@@ -73,6 +73,16 @@ export class LancamentoCadastroComponent implements OnInit {
       }),
       observacao: []
     });
+  }
+
+  validarObrigatoriedade(input: FormControl) {
+    return (input.value ? null : { obrigatoriedade: true });
+  }
+
+  validarTamanhoMinimo(valor: number) {
+    return (input: FormControl) => {
+      return (!input.value || input.value.length >= valor) ? null : { tamanhoMinimo: { tamanho: valor } };
+    };
   }
 
   get editando() {
